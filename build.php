6425f0f89da6570ce36728e70a98b8369aedc5fc
cakyus <cakyus@gmail.com>
2013-07-27 21:32:26 +0700

    parse require('hello.js');

--- build.php
+++ build.php
@@ -5,6 +5,7 @@ define('SOURCE_DIR', PROJECT_DIR.'/src');
 define('OUTPUT_DIR', PROJECT_DIR.'/build');
 define('MAIN_FILE', SOURCE_DIR.'/main.js');
 define('OUTPUT_FILE', OUTPUT_DIR.'/build.user.js');
+define('BUILD_MINI', false);
 
 // -- CODE --
 
@@ -12,23 +13,69 @@ class Builder {
 	
 	public function start() {
 		debug_print('build starting ..');
-		$content = $this->parseJavascript(MAIN_FILE);
+		$content = $this->requireJavascript(MAIN_FILE);
+		debug_print('Write output file', OUTPUT_FILE);
 		file_put_contents(OUTPUT_FILE, $content);
 		debug_print('build completed');
 	}
 	
-	public function parseJavascript($file) {
+	public function requireJavascript($file) {
 		
-		debug_print('parseJavascript '.$file);
+		debug_print('requireJavascript '.$file);
 		
 		if (is_file($file) == false) {
 			debug_print('WARNING', 'File not found');
 			return '';
 		}
 		
-		$content = file_get_contents($file);
+		return $this->parseJavascript(
+				file_get_contents($file)
+			);
+	}
+	
+	public function parseJavascript($content) {
+		return preg_replace_callback(
+			"/require\('((.+?)\.?([^\.]+)?)'\);/"
+			, array($this, 'parseRequire')
+			, $content
+			);
+	}
+	
+	
+	public function parseRequire($match) {
 		
-		return $content;
+		$file = $match[1];
+		$path = SOURCE_DIR.'/'.$match[1];
+		if (isset($match[3])) {
+			$extension = strtolower($match[3]);
+		} else {
+			$extension = '';
+		}
+		
+		debug_print('parseRequire', $path);
+		
+		if (is_file($path) == false) {
+			debug_print('File not found', $path);
+			return '';
+		}
+		
+		$content = file_get_contents($path);
+		
+		if (empty($extension)) {
+			// assume this is string
+		} elseif ($extension == 'js') {
+			if (BUILD_MINI) {
+				return $this->parseJavascript($content);
+			} else {
+				return "\n// <!-- $file\n"
+					.$this->parseJavascript($content)
+					."\n// $file -->\n"
+					;
+			}
+		} else {
+			debug_print('Unknown file extension', $extension);
+			return '';
+		}
 	}
 }
 
--- src/main.js
+++ src/main.js
@@ -24,3 +24,5 @@
 // ==/UserScript==
 
 window.stop();
+
+require('hello.js');
