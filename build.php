17f18b2ffb51d9cae643058002be74df1bd205cc
cakyus <cakyus@gmail.com>
2013-07-27 20:44:47 +0700

    add class Builder

--- build.php
+++ build.php
@@ -1,1 +1,27 @@
 <?php
+
+// -- CODE --
+
+class Builder {
+	
+	public function start() {
+		debug_print('build starting ..');
+		// @todo do something
+		debug_print('build completed');
+	}
+}
+
+
+// -- MAIN --
+
+function debug_print() {
+	echo date("H:i:s ");
+	echo implode(" ", func_get_args());
+	echo "\n";
+}
+
+$debug = debug_backtrace();
+if (empty($debug)) {
+	$builder = new Builder;
+	$builder->start();
+}
